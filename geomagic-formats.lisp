f9d99db0292164497e02b9cc0fd7fe3686cfb82b
Peter Salvi <vukung@yahoo.com>
2010-03-11 14:00:25 +0100

    RDN read/write enhancement.
    Lists are handled both for reading and writing.
    Curves can be read and written, but surfaces cannot be read yet.

--- geomagic-formats.lisp
+++ geomagic-formats.lisp
@@ -77,17 +77,88 @@
 		     :element-type '(unsigned-byte 8))
     (write-bss-stream s surface)))
 
-(defun write-rdn (surface-list filename)
+(defgeneric write-rdn-object (obj stream))
+
+(defmethod write-rdn-object ((lst cons) s)
+  (write-unsigned-integer s 1)
+  (write-unsigned-integer s 0)		; info
+  (write-unsigned-integer s 0)		; size
+  (dolist (obj lst)
+    (write-rdn-object obj s))
+  (write-unsigned-integer s 2)
+  (write-unsigned-integer s 0)		; info
+  (write-unsigned-integer s 0))		; size
+
+(defmethod write-rdn-object ((curve bspline-curve) s)
+  (write-unsigned-integer s 40) ; B-Spline Curve
+  (write-unsigned-integer s 0)  ; Length of INFO string
+  ;; INFO string goes here
+  (with-accessors ((degree degree) (knots knot-vector) (points control-points))
+      curve
+    (let ((nrknots (length knots))
+	  (nrcpts (length points)))
+      ;; Calculate the length
+      (write-unsigned-integer s (+ 4 4 4 (* nrknots 8) 4 (* 3 nrcpts 8)))
+      (write-unsigned-integer s degree)
+      (write-unsigned-integer s nrknots)
+      (write-unsigned-integer s 0)	; closedp
+      (dolist (knot (coerce knots 'list))
+	(write-double s knot))
+      (write-unsigned-integer s nrcpts)
+      (dolist (point (coerce points 'list))
+	(dolist (coordinate point)
+	  (write-double s coordinate))))))
+
+(defmethod write-rdn-object ((surface bspline-surface) s)
+  (write-unsigned-integer s 50)	; B-Spline Surface
+  (write-unsigned-integer s 0)	; Length of INFO string
+  ;; INFO string goes here
+  (let ((nrknots (mapcar #'length (knot-vectors surface)))
+	(nrcpts (apply #'* (array-dimensions (control-net surface)))))
+    ;; Calculate the length
+    (write-unsigned-integer s (+ 4 4 4 (* (first nrknots) 8)
+				 4 4 4 (* (second nrknots) 8)
+				 4 4 (* nrcpts 3 8))))
+  (write-bss-stream s surface))
+
+(defun write-rdn (lst filename)
   (with-open-file (s filename :direction :output :if-exists :supersede
 		     :element-type '(unsigned-byte 8))
-    (dolist (surface surface-list)
-      (write-unsigned-integer s 50)	; B-Spline Surface
-      (write-unsigned-integer s 0)	; Length of INFO string
-      ;; INFO string goes here
-      (let ((nrknots (mapcar #'length (knot-vectors surface)))
-	    (nrcpts (apply #'* (array-dimensions (control-net surface)))))
-	;; Calculate the length
-	(write-unsigned-integer s (+ 4 4 4 (* (first nrknots) 8)
-				     4 4 4 (* (second nrknots) 8)
-				     4 4 (* nrcpts 3 8))))
-      (write-bss-stream s surface))))
+    (dolist (obj lst)
+      (write-rdn-object obj s))))
+
+(defgeneric read-rdn-object (obj stream))
+
+(defmethod read-rdn-object ((obj (eql 'bspline-curve)) s)
+  (let* ((degree (read-unsigned-integer s))
+	 (k (read-unsigned-integer s))
+	 (closedp (read-unsigned-integer s))
+	 (knots (iter (repeat k) (collect (read-double s))))
+	 (n (read-unsigned-integer s))
+	 (points (iter (repeat n)
+		       (collect (iter (repeat 3)
+				      (collect (read-double s)))))))
+    (declare (ignore closedp))
+    (make-bspline-curve degree knots points)))
+
+(defmethod read-rdn-object ((obj (eql 'bspline-surface)) s)
+  (declare (ignore obj s))
+  'TODO)
+
+(defun read-rdn-stream (s)
+  (iter (with rdn-types = '((40 . bspline-curve) (50 . bspline-surface)))
+	(for code = (handler-case (read-unsigned-integer s)
+		      (end-of-file () (finish))))
+	(let ((info-length (read-unsigned-integer s)))
+	  (iter (repeat info-length) (read-byte s)))
+	(read-unsigned-integer s)	; size
+	(cond ((= code 2) (finish))	; EndGroup
+	      ((= code 1)		; BeginGroup
+	       (collect (read-rdn-stream s)))
+	      (t (let ((type (cdr (assoc code rdn-types))))
+		   (assert type (type) "Unknown type: ~a" code)
+		   (collect (read-rdn-object type s)))))))
+
+(defun read-rdn (filename)
+  (with-open-file (s filename :element-type '(unsigned-byte 8))
+    (read-rdn-stream s)))
