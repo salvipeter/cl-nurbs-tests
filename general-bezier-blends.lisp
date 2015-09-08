d3c25e131e0f9c04dd928b417b32cc86098a942f
Peter Salvi <vukung@yahoo.com>
2015-09-08 18:53:24 +0900

    Refactoring.

--- general-bezier-blends.lisp
+++ general-bezier-blends.lisp
@@ -1,5 +1,39 @@
 (in-package :cl-nurbs-tests)
 
+(defun generalized-bernstein (points p side degree col row &key (use-d t))
+  (let* ((n (length points))
+         (l (barycentric-coordinates points p))
+         (half-low (floor degree 2))
+         (i side)
+         (i-1 (mod (1- i) n))
+         (i+1 (mod (1+ i) n))
+         (si (barycentric-s l i))
+         (si-1 (barycentric-s l i-1))
+         (si+1 (barycentric-s l i+1))
+         (di (barycentric-d l i))
+         (di-1 (barycentric-d l i-1))
+         (di+1 (barycentric-d l i+1))
+         (alpha (if use-d
+                    (if (< (+ di-1 di) *epsilon*)
+                        0.5
+                        (/ di-1 (+ di-1 di)))
+                    (if (< (+ si (- 1 si-1)) *epsilon*)
+                        0.5
+                        (/ si (+ si (- 1 si-1))))))
+         (beta (if use-d
+                   (if (< (+ di+1 di) *epsilon*)
+                       0.5
+                       (/ di+1 (+ di+1 di)))
+                   (if (< (+ (- 1 si) si+1) *epsilon*)
+                       0.5
+                       (/ (- 1 si) (+ (- 1 si) si+1)))))
+         (blend (* (bernstein degree row di)
+                   (bernstein degree col si)))
+         (mu (cond ((and (evenp degree) (= col half-low)) 1)
+                   ((> col half-low) beta)
+                   (t alpha))))
+    (* blend mu)))
+
 ;;; DEFICIENCY => see deficiency.lisp
 (defun write-bernstein-blend-mesh (path n degree &key (use-d t))
   (let ((points (points-from-angles (uniform-angles n))))
@@ -9,40 +43,8 @@
                 (with-open-file (s fname :direction :output :if-exists :supersede)
                   (write-obj-indexed-mesh
                    (iter (for p in (vertices points))
-                         (for l = (barycentric-coordinates points p))
-                         (for half-low = (floor degree 2))
-                         (for half-up = (ceiling degree 2))
-                         (for i = 0)    ; use always side 0
-                         (for i-1 = (mod (1- i) n))
-                         (for i+1 = (mod (1+ i) n))
-                         (for si = (barycentric-s l i))
-                         (for si-1 = (barycentric-s l i-1))
-                         (for si+1 = (barycentric-s l i+1))
-                         (for di = (barycentric-d l i))
-                         (for di-1 = (barycentric-d l i-1))
-                         (for di+1 = (barycentric-d l i+1))
-                         (for alpha =
-                              (if use-d
-                                  (if (< (+ di-1 di) *epsilon*)
-                                      0.5
-                                      (/ di-1 (+ di-1 di)))
-                                  (if (< (+ si (- 1 si-1)) *epsilon*)
-                                      0.5
-                                      (/ si (+ si (- 1 si-1))))))
-                         (for beta =
-                              (if use-d
-                                  (if (< (+ di+1 di) *epsilon*)
-                                      0.5
-                                      (/ di+1 (+ di+1 di)))
-                                  (if (< (+ (- 1 si) si+1) *epsilon*)
-                                      0.5
-                                      (/ (- 1 si) (+ (- 1 si) si+1)))))
-                         (for blend = (* (bernstein degree row di)
-                                         (bernstein degree col si)))
-                         (for mu = (cond ((and (evenp degree) (= col half-low)) 1)
-                                         ((< col half-up) alpha)
-                                         ((> col half-low) beta)))
-                         (collect (cons (* blend mu) p)))
+                         (for b = (generalized-bernstein points p 0 degree col row :use-d use-d))
+                         (collect (cons b p)))
                    (triangles n)
                    fname))))
     (let ((fname (format nil "~a/~asided-deg~a-center.obj" path n degree)))
