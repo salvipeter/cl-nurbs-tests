cf9887c896ddde2062cc6cf5237b7e6d464a28f2
Peter Salvi <vukung@yahoo.com>
2016-09-29 18:14:37 +0200

    Computing best rational approximations.

--- gb-find-optimal-delta.lisp
+++ gb-find-optimal-delta.lisp
@@ -39,3 +39,31 @@
            (let ((*barycentric-dilation* x))
              (tangent-error n))))
     (golden-search #'f min max iterations)))
+
+(defun continued-fraction (x n)
+  (iter (repeat n)
+        (for i = (floor x))
+        (collect i)
+        (setf x (/ (- x i)))))
+
+(defun eval-continued-fraction (f)
+  (cond ((null f) 0)
+        ((null (rest f)) (first f))
+        (t (+ (first f) (/ (eval-continued-fraction (rest f)))))))
+
+(defun best-rational-approximations (x n)
+  "Returns a list of best rational approximations of X.
+Each element is a pair of the approximation and its error.
+N is the length of the continued fraction used in the algorithm."
+  (iter (with f = (continued-fraction x n))
+        (for j from 1 to n)
+        (for fr = (reverse (subseq f 0 j)))
+        (for p = (first fr))
+        (for ok = 
+             (or (oddp p)
+                 (> (abs (- x (eval-continued-fraction (reverse (rest fr)))))
+                    (abs (- x (eval-continued-fraction (reverse (cons (/ p 2) (rest fr)))))))))
+        (appending
+         (iter (for i from (+ (ceiling p 2) (if ok 0 1)) to p)
+               (for y = (eval-continued-fraction (reverse (cons i (rest fr)))))
+               (collect (cons y (abs (- x y))))))))
