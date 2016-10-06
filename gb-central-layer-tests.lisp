5f73d805993314fe0c85e1c604bdff1dbf13b9f1
Peter Salvi <vukung@yahoo.com>
2016-10-06 08:36:33 +0200

    Non-diagonal parameterization.

--- gb-central-layer-tests.lisp
+++ gb-central-layer-tests.lisp
@@ -57,6 +57,60 @@
                           (/ 4))))
                (sum blf-sum))))))
 
+(defun deficiency-squared-central-layer-nondiagonal (n degree &key (position 'center) (use-d t))
+  (let* ((points (points-from-angles (uniform-angles n)))
+         (p (case position
+              (center '(0 0))
+              (edge-center-mid
+               (v* (v+ (first points) (second points)) 1/4))
+              (vertex-center-mid
+               (v* (first points) 1/2))
+              (t position)))
+         (l (barycentric-coordinates points p)))
+    (flet ((sqr (x) (* x x)))
+      (- 1
+         (iter (for i from 0 below n)
+               (for i-1 = (mod (1- i) n))
+               (for i+1 = (mod (1+ i) n))
+               (for si = (barycentric-s l i))
+               (for si-1 = (barycentric-s l i-1))
+               (for si+1 = (barycentric-s l i+1))
+               (for di = (barycentric-d l i))
+               (for di-1 = (barycentric-d l i-1))
+               (for di+1 = (barycentric-d l i+1))
+               (for alpha =
+                    (if use-d
+                        (if (< (+ di-1 di) *epsilon*)
+                            0.5
+                            (/ (sqr di-1) (+ (sqr di-1) (sqr di))))
+                        (if (< (+ si (- 1 si-1)) *epsilon*)
+                            0.5
+                            (/ (sqr si) (+ (sqr si) (sqr (- 1 si-1)))))))
+               (for beta =
+                    (if use-d
+                        (if (< (+ di+1 di) *epsilon*)
+                            0.5
+                            (/ (sqr di+1) (+ (sqr di+1) (sqr di))))
+                        (if (< (+ (- 1 si) si+1) *epsilon*)
+                            0.5
+                            (/ (sqr (- 1 si)) (+ (sqr (- 1 si)) (sqr si+1))))))
+               (for blf-sum = 0)
+               (iter (with layers = (ceiling degree 2))
+                     (for row from 0 below layers)
+                     (iter (for col from 0 to degree)
+                           (for blend = (* (bernstein degree row di)
+                                           (bernstein degree col si)))
+                           (for mu = (cond ((< col layers) alpha)
+                                           ((> col (- degree layers)) beta)
+                                           (t 1)))
+                           (incf blf-sum (* mu blend))))
+               (when (evenp degree)
+                 (incf blf-sum
+                       (* (bernstein degree (/ degree 2) di)
+                          (bernstein degree (/ degree 2) si)
+                          (/ 4))))
+               (sum blf-sum))))))
+
 #+nil
 (let ((*resolution* 2))
   (format t "Squared alpha/beta, central layer:~%|n|d|original|plus layer|negative|monotonic|~%")
--- gb-delta-fullness.lisp
+++ gb-delta-fullness.lisp
@@ -26,3 +26,26 @@
     (flet ((li (i) (elt l (mod i n))))
       (/ (- h 0.5) (li -2) (li 1)
          (- 1 (li -2) (li -1) (li 0) (li 1))))))
+
+(defun find-point-on-05-with-h (points h)
+  (bisection-search-root (lambda (x)
+                           (- (barycentric-d (barycentric-coordinates points `(0 ,x)) 0) h))
+                         -1 0.0 100.0))
+
+#+nil
+(defun extended-cp-ratio (n d)
+  (let* ((*barycentric-dilation* (find-fullness-delta n))
+         (points (points-from-angles (cons (+ 270 (/ 180 n)) (rest (uniform-angles n)))))
+         (h (- 0.5 (/ d)))
+         (h-cp (barycentric-d (barycentric-coordinates points '(0 0)) 0))
+         (p05 (find-point-on-05-with-h points 0.5))
+         (ph  (find-point-on-05-with-h points h))
+         (pcp (find-point-on-05-with-h points h-cp)))
+    (/ (- p05 ph) (- pcp ph))))
+
+(defun extended-cp-ratio (n d)
+  (let* ((*barycentric-dilation* (find-fullness-delta n))
+         (points (points-from-angles (cons (+ 270 (/ 180 n)) (rest (uniform-angles n)))))
+         (h (- 0.5 (/ d)))
+         (h-cp (barycentric-d (barycentric-coordinates points '(0 0)) 0)))
+    (/ (- 0.5 h) (- h-cp h))))
